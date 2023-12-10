9ea50d59bef5284939dce9291e55615aa595a0f8
Chiara Puleio <chiarapuleio@yahoo.it>
2023-12-10 12:34:54 +0100

    footer, project completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { AuthGuard } from './auth/auth.guard';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Route[] = [
   {
@@ -52,6 +53,7 @@ const routes: Route[] = [
     NavbarComponent,
     ProfileComponent,
     MoviesComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
